caab8afde21db0dbb4368be967afd4eaece1a160
Hassan Juma <okothhassanjuma@gmail.com>
2020-10-26 14:07:13 +0300

    add config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://user:kopudoju@localhost/pitch_platform'
+# SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://hassan:kopudoju@localhost/pitch_platform'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
-    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
@@ -27,7 +27,7 @@ class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://:user:kopudoju@localhost/pitch_platform_test'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://:hassan:kopudoju@localhost/pitch_platform_test'
 
 
 class DevConfig(Config):
@@ -37,7 +37,7 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://user:kopudoju@localhost/pitch_platform'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://hassan:kopudoju@localhost/pitch_platform'
     DEBUG = True
 
 config_options = {
