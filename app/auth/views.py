82c2d2c749cb60622527f4a9cb88a3dc3dad9db9
Hassan Juma <okothhassanjuma@gmail.com>
2020-10-28 10:09:39 +0300

    add

--- app/auth/views.py
+++ app/auth/views.py
@@ -14,7 +14,7 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        mail_message("Welcome to Pitch platform","email/welcome_user",user.email,user=user)
+        message = mail_message("Welcome to Pitch platform","email/welcome_user",user.email,user=user)
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)    
