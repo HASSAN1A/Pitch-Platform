33517091d56cfb059cd0f7de50af8301585beb9f
Hassan Juma <okothhassanjuma@gmail.com>
2020-10-28 10:09:25 +0300

    add

--- app/email.py
+++ app/email.py
@@ -8,4 +8,8 @@ def mail_message(subject,template,to,**kwargs):
     email = Message(subject, sender=sender_email, recipients=[to])
     email.body= render_template(template + ".txt",**kwargs)
     email.html = render_template(template + ".html",**kwargs)
-    mail.send(email)
+    try:
+      mail.send(email)
+    except:
+        print("hey")
+        return {"message":"mail me later"}
